f09e6010d26cfafbf33c708d9aeee1b49026033f
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-27 13:02:34 -0500

    complete backend weather

--- server.js
+++ server.js
@@ -1,44 +1,40 @@
 /* eslint-disable no-undef */
 'use strict';
 console.log('server.js is connected!');
-
-
 const express = require('express');
 require('dotenv').config();
 let weatherData = require('./data/weather.json');
-
 const cors = require('cors');
-
-
-//2.
 const app = express();
 app.use(cors());
+
 const PORT = process.env.PORT || 5005;
 
-
 app.get('/', (request, response) => {
-  //then we need to send something back.
   response.send('hello from our server!!');
 });
 
-
 //add a route
-app.get('/weather', (request, response) => {
-  try {
-    // let searchQuery = request.query.searchQuery;
-    // let dataToConstructor = weatherData.find(weather => weather.city_name.toLowerCase() === searchQuery.toLowerCase());
-    // let dataSending =  dataToConstructor.data.map(dayForecast => new Forecast(dayForecast));
-    response.send(dataSending);
-  } catch(error) {
-    next(error);
-  }
+app.get('/weather', getWeather);
+// app.get('/movies', getMovies);
+// app.get('/yelp', getYelp);
+
+async function getWeather(request, response) {
+let { latitude, longitude } = request.query;
+let url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&days=5&aqi=no&alerts=no&q=${latitude},${longitude}`;
+
+let weatherData = await axios.get(url);
+let weatherSummaries = weatherData.data.forecast.forecastday.map(day => {
+  return new Forecast(day);
 });
+response.status(200).send(weatherSummaries);
+}
 
 
 class Forecast {
-  constructor(weatherObjects) {
-    this.date = weatherObjects.valid_date;
-    this.description = weatherObjects.weather.description;
+  constructor(forecastObjects) {
+    this.date = forecastObjects.date;
+    this.forecast = forecastObject.day.condition.text;
   }
 }
 
