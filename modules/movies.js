21212609705035e34b1089d5ed2afa6ebd47fb08
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-29 10:01:34 -0500

    fix map and weatherData, trying

--- modules/movies.js
+++ modules/movies.js
@@ -8,11 +8,11 @@ module.exports = getMovies;
 
 // Movie route
 async function getMovies(request, response) {
-    let { latitude, longitude } = request.query;
+    let { searchQuery } = request.query;
     let url = `https://api.themoviedb.org/3/search/movie?api_key=45b661bcbcf607acc1df316673e4b463&language=en-US&query=seattle&page=1&include_adult=false`;
 
     let cityMovie = await axios.get(url)
-    let movieArray = cityMovie.data.movie.map(movieData => new Movie(movieData));
+    let movieArray = cityMovie.data.movieData.map(movieData => new Movie(movieData));
     response.status(200).send(movieArray)
 }
 
